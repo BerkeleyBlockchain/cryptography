5f9b7296f58b330ec2ba4605760ce3820f43b7b9
Tim Huang <timothy.huang@berkeley.edu>
2018-10-21 13:13:46 -0700

    merge conflict

--- secret-sharing/secret-sharing.py
+++ secret-sharing/secret-sharing.py
@@ -1,5 +1,10 @@
 """secret-sharing.py"""
-import random
+
+import sys
+sys.path.append('../utils')
+
+from primes import *
+from mod import *
 
 class SecretShare:
 
@@ -9,53 +14,64 @@ class SecretShare:
 		self.num_shares = num_shares
 		generate_random_shares(secret, minimum, num_shares)
 
+    def poly_eval(poly, x):
+        '''Evaluate a polynomial (represented as a list) at x
+        >>> poly_eval([1, 2, 3], 4)
+        57
+        >>> poly_eval([50, 60, 70], 2)
+        450
+        '''
+        total, i = 0, 0
+        for coef in poly:
+            total += coef * pow(x, i)
+            i += 1
+        return total
 
+    def poly_add(a, b):
+        '''
 
-	def div_mod(num, denom, p):
-	    '''Compute num / denom in modulo p
+        '''
+        result = max((a,b), key=len)
+        for i in range(min(len(a), len(b))):
+            result[i] = (a[i] + b[i])
+        return result
 
-	    >>> div_mod(3, 5, 2)
-	    1
+    print(poly_add([1, 2], [3, 4]))
+    print(poly_add([1, 2, 3, 4], [5, 6]))
 
-	    '''
+    def poly_scalar_mul(p, c):
+        for i in range(len(p)):
+            p[i] = p[i] * c
 
-	    inv = find_inverse(p, denom)
-	    return num * inv
+    def poly_mul(a, b):
+        i, z = 0, []
+        for x in b:
+            n = []
+            for i in range(i):
+                n.append(0)
+            for j in range(len(a)):
+                n.append(x * a[j])
+            z.append(n)
+            i += 1
+        v = []
+        for x in z:
+            v = poly_add(v, x)
+        return v
 
+    print(poly_mul([1, 2], [2, 3]))
 
-	def generate_random_shares(secret, minimum, num_shares):
+    print(poly_mul([1, 2, 3, 4], [8, 9]))
 
-	    assert minimum < shares, "Minimum shares must be less than total shares)"
-	    poly = [secret] + [generate_prime() for i in range(minimum)]
+        #[1,2]      [2, 3]
+        #1 + 2x     2 + 3x
 
-	    shares = [(i, poly_eval(poly, i)) for i in range(1, num_shares + 1)]
+        #2 + 7x + 6x^2
 
-	    return shares
-
-
-	def poly_eval(poly, x):
-	    '''Evaluate a polynomial (represented as a list) at x
-
-	    >>> poly_eval([1, 2, 3], 4)
-	    57
-
-	    >>> poly_eval([50, 60, 70], 2)
-	    450
-
-	    '''
-	    sum, i = 0, 0
-	    for coef in poly:
-	    	sum += coef * pow(x, i)
-	    	i += 1
-	    return sum
-
-	def lagrange_interpolation(pairs):
-		'''Find the polynomial P, with degree i -1 given a set of i number of points in pairs'''
-		list_of_x = []
-		list_of_y = []
-		for x in len(pairs):
-			list_of_x = list_of_x + x[0]
-		for y in len(pairs):
-			list_of_y = list_of_y + y[1]
-
-
+    def lagrange_interpolation(pairs):
+        '''Find the polynomial P, with degree i -1 given a set of i number of points in pairs'''
+        list_of_x = []
+        list_of_y = []
+        for x in len(pairs):
+            list_of_x = list_of_x + x[0]
+        for y in len(pairs):
+            list_of_y = list_of_y + y[1]
