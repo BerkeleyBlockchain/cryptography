36a2c0814e254964d6b5e6838b1b0d5abaa0e390
Tim Huang <timothy.huang@berkeley.edu>
2018-10-21 12:05:27 -0700

    initial secret sharing implementation

--- secret-sharing/secret-sharing.py
+++ secret-sharing/secret-sharing.py
@@ -1,1 +1,17 @@
 
+def div_mod(num, denom, p):
+    '''Compute num / denom in modulo p
+
+    >>> div_mod(3, 5, 2)
+    1
+
+    '''
+
+    inv = find_inverse(p, denom)
+    return num * inv
+
+
+def generate_random_shares(secret, minimum, shares):
+
+    assert minimum < shares, "Minimum shares must be less than total shares)"
+    poly = [secret] + [randomint() for i in range(minimum)]
