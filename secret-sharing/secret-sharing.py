21789337bdaf519d8d5915d8100d7d376fcc70a5
Tim Huang <timothy.huang@berkeley.edu>
2018-10-21 12:24:55 -0700

    updated generate_random_shares, added poly_eval

--- secret-sharing/secret-sharing.py
+++ secret-sharing/secret-sharing.py
@@ -1,4 +1,6 @@
 
+import random
+
 def div_mod(num, denom, p):
     '''Compute num / denom in modulo p
 
@@ -11,7 +13,29 @@ def div_mod(num, denom, p):
     return num * inv
 
 
-def generate_random_shares(secret, minimum, shares):
+def generate_random_shares(secret, minimum, num_shares):
 
     assert minimum < shares, "Minimum shares must be less than total shares)"
-    poly = [secret] + [randomint() for i in range(minimum)]
+    poly = [secret] + [generate_prime() for i in range(minimum)]
+
+    shares = [(i, poly_eval(poly, i)) for i in range(1, num_shares + 1)]
+
+    return shares
+
+
+def poly_eval(poly, x):
+    '''Evaluate a polynomial (represented as a list) at x
+
+    >>> poly_eval([1, 2, 3], 4)
+    57
+
+    >>> poly_eval([50, 60, 70], 2)
+    450
+
+    '''
+
+    sum, i = 0, 0
+    for coef in poly:
+        sum += coef * pow(x, i)
+        i += 1
+    return sum
