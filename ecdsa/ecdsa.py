45da378dbd4911dc5d11c74904bacf29c823dc9c
justinvyu <justin.v.yu@gmail.com>
2018-11-11 14:35:33 -0800

    Add ECDSA class

--- ecdsa/ecdsa.py
+++ ecdsa/ecdsa.py
@@ -1,4 +1,11 @@
 """ecdsa.py"""
 
 class ECDSA:
+    """
+    ECDSA (Elliptic Curve Digital Signature Algorithm)
+    Implementation of an ECDSA key generation scheme.
+    """
+
+    # def __init__():
+    #     pass
 
