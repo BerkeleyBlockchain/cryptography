4e71de8d7577daefba68357367d7c2865e8f1f2f
justinvyu <justin.v.yu@gmail.com>
2018-11-11 14:59:12 -0800

    Add working point addition

--- ecdsa/curve.py
+++ ecdsa/curve.py
@@ -15,23 +15,24 @@ class Curve:
         self.b = b
         self.history = [self.get_point(randint(1, 100))]
 
-
     def get_point(self, x):
-        y = pow(pow(x, 3) + a*x + b, 0.5)
+        y = pow(pow(x, 3) + self.a * x + self.b, 0.5)
         return (x, y)
 
-    def add_point():
+    def add_point(self):
+        point1 = self.history[0]
+        point2 = self.history[0]
+        m = 0
 
-        if len(history) == 1:
-            ...
-
+        if len(self.history) == 1:
+            m = (3 * pow(point1[0], 2) + self.a) / (2 * point1[1])
         else:
-            point1, point2 = self.history[0], self.history[len(self.history - 1)]
+            point2 = self.history[len(self.history - 1)]
             m = (point1[1] - point2[1]) / (point1[0] - point2[0])
 
-            x = pow(m, 2) - point1[0] - point2[0]
-            y = point1[1] + m * (x - point1[0])
+        x = pow(m, 2) - point1[0] - point2[0]
+        y = -(point1[1] + m * (x - point1[0]))
 
-            point = (x, y)
-            self.history.append(point)
-            return point
+        point = (x, y)
+        self.history.append(point)
+        return point
