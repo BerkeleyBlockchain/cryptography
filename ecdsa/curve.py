3c413e3f45b0399fe5373fae0a612e3ba86e5a63
Tim Huang <timothy.huang@berkeley.edu>
2018-11-11 14:45:37 -0800

    began add_point

--- ecdsa/curve.py
+++ ecdsa/curve.py
@@ -1,5 +1,7 @@
 
 
+from random import randint
+
 class Curve:
 
     """
@@ -11,5 +13,18 @@ class Curve:
     def __init__(self, a , b):
         self.a = a
         self.b = b
+        self.history = [self.get_point(randint(1, 100))]
+
 
     def get_point(self, x):
+        y = pow(pow(x, 3) + a*x + b, 0.5)
+        return (x, y)
+
+    def add_point():
+
+        if len(history) == 1:
+            ...
+
+        else:
+            point1, point2 = self.history[0], self.history[len(self.history - 1)]
+            slope = (point1[0] - point2[0]) / (point1[1] - point2[1])
